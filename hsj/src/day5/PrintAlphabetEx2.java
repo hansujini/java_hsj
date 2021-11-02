e570555f47d64a43c743f98966d5f5fed44dc52b
hansujin <qweii7975@naver.com>
2021-11-02 16:25:50 +0900

    5-4. While문 알파벳 예제

---
+++ hsj/src/day5/PrintAlphabetEx2.java
@@ -0,0 +1,32 @@
+package day5;
+
+public class PrintAlphabetEx2 {
+
+	public static void main(String[] args) {
+		/* a		시작 : a 끝 : a
+		 * ab		시작 : a 끝 : b
+		 * abc		시작 : a 끝 : c
+		 * abcd		시작 : a 끝 : d
+		 * ....			
+		 * abcd...xyz  시작 : a 끝 : z
+		 * 가 출력되도록 코드를 작성하세요.
+		 * 반복횟수 :ch는 a부터 z까지 1씩 증가
+		 * 규칙성 : a부터 ch까지 출력 후 앤터
+		 *     반복횟수 : a부터 ch까지 출력
+		 *     규칙성 : ch2를 출력
+		 *     앤터 출력
+		 * */
+		//ch가 끝문자에 기준이됨
+
+		char ch, ch2;
+		for(ch='a'; ch<='z'; ch++) {
+			for(ch2='a'; ch2<=ch; ch2++) {
+				System.out.print(ch2);
+			}
+			System.out.println();
+			
+			
+		}
+		
+	}
+}
