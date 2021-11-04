33ce3c0344979d7725717fc294b80970194e080b
kyg <93393540+sky96027@users.noreply.github.com>
2021-11-04 17:13:35 +0900

    7-10: 메소드 매개변수 배열 예제

---
+++ hsj/src/day7/MethodArrayEx1.java
@@ -0,0 +1,53 @@
+package day7;
+
+public class MethodArrayEx1 {
+	//매개 변수가 참조변수인 경우 => 매개 변수가 배열인 경우
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]arr = {2, 1, 3};
+		printArray(arr);
+		//System.out.println(arr[0]);
+		//initArray(arr);
+		sortArray(arr);
+		printArray(arr);
+	}
+	/* 가능 : 정수형 배열이 주어진 해당 배열의 모든 값을 한줄로 콘솔에 출력하는 메소드
+	 * 매개변수 : 정수형 배열 => int[]arr
+	 * 리턴타입 : 없음. => void
+	 * 메소드명 : printArray
+	 * */
+	public static void printArray(int []arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+ " ");
+		}
+		System.out.println();
+		//arr[0] = 10;// 이 코드를 활성화하면 원본 배열의 값이 변경됨
+	}
+	/* 기능 : 정수형 배열이 주어지면 주어진 배열의 모든 값을 0으로 초기화 하는 메소드
+	 * 매개변수 : 정수형 배열 => int[] arr
+	 * 리턴타입 : 없음. = void
+	 * 메소드명 : initArray
+	 * */
+	public static void initArray(int[] arr) {
+		for(int i=0; i<arr.length;i++) {
+			arr[i] = 0;
+		}
+	}
+	/* 메소드 안에 있는 부분은 구현하지 않아도 됨.
+	 * 기능 : 정수형 배열이 주어지면 주어진 배열을 정렬하는 메소드
+	 * 매개변수 : 정수형 배열 => int [] arr
+	 * 리턴타입 : 없음 = void
+	 * 메소드명 : sortArray
+	 * */
+	public static void sortArray (int[]arr) {
+		for(int i = 0; i<arr.length-1; i++) {
+			for(int j = 0; j<arr.length-i-1;j++) {
+				if(arr[j] > arr[j+1]) {  // 순서를 바꾸고 싶으면 부등호 방향을 바꾸면됨. 오름차순 내림차순
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+	}
+}
