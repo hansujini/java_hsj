50ca86709c48e97d5271089833f032a0fd500c71
kyg <93393540+sky96027@users.noreply.github.com>
2021-11-04 17:09:33 +0900

    7-2 : 배열에 랜덤 수 저장 예제

---
+++ hsj/src/day7/ArrayRandomEx1.java
@@ -0,0 +1,43 @@
+package day7;
+
+import java.util.Arrays;
+
+public class ArrayRandomEx1 {
+
+	public static void main(String[] args) {
+		/* 1~9사이의 랜덤한 수를 3개 생성하여 배열에 저장하는 출력하는 코드를 작성하세요*/
+		 
+		//3개짜리 배열을 생성
+		int num[] = new int[3];
+		int min = 1, max = 9;
+		
+		//3번반복
+		for(int i = 0; i<num.length; i++) {
+				//랜덤한 수를 생성
+				int random = (int)(Math.random()*(max-min+1)+min);
+				//배열에 랜덤한 수를 저장
+				num[i] = random;
+				
+		}
+		//배열을 출력
+		System.out.println(Arrays.toString(num));
+			
+		/* 반법1. 배열x 반복문x
+		int num1, num2, num3;
+		int min = 1, max = 9;
+		num1 = (int)(Math.random()*(max-min+1)+min);
+		num2 = (int)(Math.random()*(max-min+1)+min);
+		num3 = (int)(Math.random()*(max-min+1)+min);
+		System.out.println(num1 + "," + num2 + "," + num3);
+		*/
+		//방법2. 배열2, 반복문x
+		/*int num[] = new int[3];
+		int min = 1, max = 9;
+		num[0] = (int)(Math.random()*(max-min+1)+min);
+		num[1] = (int)(Math.random()*(max-min+1)+min);
+		num[2] = (int)(Math.random()*(max-min+1)+min);
+		System.out.println(Arrays.toString(num));
+		*/
+	}
+
+}
